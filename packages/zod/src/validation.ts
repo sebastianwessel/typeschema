38b0d7ab194ab09a80673e3f869b533c113c4a9d
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2025-06-20 21:53:08 +0100

    chore: reduce code complexity

--- packages/zod/src/validation.ts
+++ packages/zod/src/validation.ts
@@ -1,6 +1,6 @@
 import type {AdapterResolver} from './resolver';
 import type {ValidationAdapter} from '@typeschema/core';
-import type {ZodError as ZodErrorV3, ZodType as ZodTypeV3} from 'zod/v3';
+import type {ZodType as ZodTypeV3} from 'zod/v3';
 import type {$ZodType as ZodTypeV4} from 'zod/v4/core';
 
 import {safeParseAsync as safeParseAsyncV4} from 'zod/v4/core';
@@ -10,27 +10,20 @@ export const validationAdapter: ValidationAdapter<
 > = async schema => {
   return async data => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    if ((schema as any)._zod) {
-      const result = await safeParseAsyncV4(schema as ZodTypeV4, data);
-      if (result.success) {
-        return {data: result.data, success: true};
-      }
-      return {
-        issues: result.error.issues.map(({message, path}) => ({message, path})),
-        success: false,
-      };
-    } else {
-      const result = await (schema as ZodTypeV3).safeParseAsync(data);
-      if (result.success) {
-        return {data: result.data, success: true};
-      }
-      return {
-        issues: (result.error as ZodErrorV3).issues.map(({message, path}) => ({
-          message,
-          path,
-        })),
-        success: false,
-      };
+    const isZodV4 = (schema as any)._zod;
+
+    const result = isZodV4
+      ? await safeParseAsyncV4(schema as ZodTypeV4, data)
+      : await (schema as ZodTypeV3).safeParseAsync(data);
+
+    if (result.success) {
+      return {data: result.data, success: true};
     }
+
+    const issues = result.error.issues.map(({message, path}) => ({
+      message,
+      path,
+    }));
+    return {issues, success: false};
   };
 };
